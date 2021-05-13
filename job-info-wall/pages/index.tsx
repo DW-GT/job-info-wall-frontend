f7cccc44ede2843a8ea5a845a579f4e75bb44b13
David Wögerbauer <david.woegerb@gmail.com>
2021-05-13 13:19:33 +0200

    Revert "trying to add applications"
    This reverts commit 0ddea3a9415244bbc3b324888a945ed89af3fe99.

--- job-info-wall/pages/index.tsx
+++ job-info-wall/pages/index.tsx
@@ -9,32 +9,12 @@ import { Headline } from '../components/Headline';
 import { Header } from '../components/Header';
 import { Navigation } from '../components/Navigation';
 import { Applications } from '../components/Applications';
-import { GetStaticProps } from 'next';
 
-const MainPage = ({applications,applicationTypes}) => {
-    return(
-    
+const MainPage = () => (
     <ThemeProvider theme={theme}>
         <Navigation navigationText="Jobs"></Navigation>
-        <Applications typeID="0" applications={applications} applicationTypes= {applicationTypes}></Applications>
+        <Applications></Applications>
     </ThemeProvider>
-)};
+);
 
 export default MainPage;
-
-export const getStaticProps: GetStaticProps = async() => {
-
-    const resTypes = await fetch(`http://localhost:4000/api/application/getOfferTypes`);
-    const applicationTypes = await resTypes.json();
-
-    const res = await fetch(`http://localhost:4000/api/application/getAllOffers`);
-    const applications = await res.json();
-
-    return {
-        props:{
-            applications,
-            applicationTypes
-        }
-    }
-
-}
